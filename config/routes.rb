88415176f4b59bc844b6b2c000351528067a651e
Juan Andrew _test_ <juandreww@outlook.com>
2022-11-27 12:47:18 +0700

    asdfasdf

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get '/articles', to: 'articles#index'
   get '/articles/:id', to: 'articles#show'
+  resources :articles
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
