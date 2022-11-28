9e435c1227de5d10efbd6b8d03faf6777ae1d3fa
Juan Andrew _test_ <juandreww@outlook.com>
2022-11-28 08:25:18 +0700

    asdfasf

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: params['article']['title'], body: params['article']['body'])
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
