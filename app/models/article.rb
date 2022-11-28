159b1222df07053647257e70a09c49d135349463
Juan Andrew _test_ <juandreww@outlook.com>
2022-11-28 07:56:40 +0700

    dry

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,10 @@ class Article < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  status == 'archived'
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :article
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  status == 'archived'
 end
