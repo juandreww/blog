03a293caef54200d0abda01cce6ff3deaeccdbeb
Juan Andrew _test_ <juandreww@outlook.com>
2022-11-28 08:20:07 +0700

    public count

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,8 @@
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
-
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  status == 'archived'
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,5 @@
 class Comment < ApplicationRecord
+  include Visible
+
   belongs_to :article
-
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  status == 'archived'
 end
