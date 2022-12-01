a9f9a3c953a51586bcb94604b6adc37c836ea6c7
Juan Andrew _test_ <juandreww@outlook.com>
2022-12-01 07:33:27 +0700

    asdfasdf

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,11 @@ class Article < ApplicationRecord
   include Visible
 
   has_many :comments, dependent: :destroy
+  validates_associated :comments
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
-  validates :eula, acceptance: true
+  validates :eula, acceptance: {
+    message: 'must be abided'
+  }
 end
