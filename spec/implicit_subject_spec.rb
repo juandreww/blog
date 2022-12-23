7aaf1da25a93d86a5a9283ee7adf39573ae9461f
Juan Andrew _test_ <juandreww@outlook.com>
2022-12-23 07:09:11 +0700

    asdfasdf

--- spec/implicit_subject_spec.rb
+++ spec/implicit_subject_spec.rb
@@ -5,8 +5,10 @@ RSpec.describe Array do
   it 'try to test subject' do
     expect(subject.length).to eq(0)
     expect(subject.class).to eq(Array)
-    subject.push('yakitori')
-    # expect(subject.length).to eq(1)
-    # expect(subject[0]).to eq('yakitori')
+
+    array = subject.push('yakitori')
+
+    expect(array.length).to eq(1)
+    expect(array[0]).to eq('yakitori')
   end
 end
