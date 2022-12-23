acc248cf2b74de7bccc61a86b574ac4b8adb1143
Juan Andrew _test_ <juandreww@outlook.com>
2022-12-23 07:00:23 +0700

    asdfasdf

--- spec/math_calculation_assignment_spec.rb
+++ spec/math_calculation_assignment_spec.rb
@@ -1,28 +1,48 @@
 require "rails_helper"
 require "byebug"
 
-RSpec.describe "regular hooks" do
+RSpec.describe "multi context hooks" do
   before(:context) do
-    puts "A"
+    puts "K"
   end
 
   before(:example) do
-    puts "C"
+    puts "Z"
   end
 
   after(:example) do
-    puts "D"
+    puts "E"
   end
 
-  after(:context) do
-    puts "B"
+  it "can multiply" do
+    expect(3 * 7).to eq(21)
   end
 
-  it "should compare strings" do
-    expect("hello").to eq("hello")
+  it "can divide" do
+    expect(40 / 8).to eq(5)
   end
 
-  it("should compare numbers") do
-    expect(3.14).to eq(3.14)
+  context "with some criteria" do
+    before(:context) do
+      puts "H"
+    end
+
+    before(:example) do
+      puts "B"
+    end
+
+    it "can subtract" do
+      expect(10 - 5).to eq(5)
+    end
+
+    context "in this condition" do
+      before(:example) do
+        puts "V"
+      end
+
+      it "can add" do
+        expect(8 + 6).to eq(14)
+      end
+    end
   end
 end
