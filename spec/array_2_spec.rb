ba12cfefd6cbaa37134509d52759b36c3f14a2cd
Juan Andrew _test_ <juandreww@outlook.com>
2022-12-28 07:19:26 +0700

    asdfasf

---
+++ spec/array_2_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+require "byebug"
+
+RSpec.describe Array do
+  subject(:sally) do
+    [100, 108]
+  end
+
+
+  it 'has two key value pairs' do
+    expect(subject.length).to eq(2)
+    subject.pop
+    expect(subject.length).to eq(1)
+  end
+
+  it 'has the same element with sally' do
+    expect(sally).to eq(subject)
+    expect(sally.length).to eq(2)
+  end
+end
