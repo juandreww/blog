4b40b313634ff079ce68923b79771feccb6ca02f
Juan Andrew _test_ <juandreww@outlook.com>
2022-12-29 07:09:38 +0700

    louis name

--- spec/described_class_spec.rb
+++ spec/described_class_spec.rb
@@ -7,9 +7,11 @@ class King
 end
 
 RSpec.describe King do
-  subject { King.new('Boris') }
+  subject { described_class.new('Boris') }
+  let(:louis) { described_class.new('Louis') }
 
   it 'represents a good person' do
     expect(subject.name).to eq('Boris')
+    expect(louis.name).to eq('Louis')
   end
 end
