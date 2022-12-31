d024f581d4895f6cac221be89a029cd1ffd24a86
Juan Andrew _test_ <juandreww@outlook.com>
2022-12-31 21:51:09 +0700

    asdfasf

---
+++ spec/have_attributes_matcher_spec.rb
@@ -0,0 +1,24 @@
+class ProfessionalWrestler
+  attr_reader :name, :finishing_move
+
+  def initialize(name, finishing_move)
+    @name = name
+    @finishing_move = finishing_move
+  end
+end
+
+RSpec.describe 'have_attributes matcher' do
+  context ProfessionalWrestler.new('Stone World', 'Science') do
+    it 'checks for object attribute and proper values' do
+      expect(subject).to have_attributes(name: 'Stone World')
+      expect(subject).to have_attributes(name: 'Stone World', finishing_move: 'Science')
+    end
+
+    it { is_expected.to have_attributes(name: 'Stone World') }
+    it { is_expected.to have_attributes(name: 'Stone World', finishing_move: 'Science') }
+  end
+end
+
+RSpec.describe 'pokemon attributes' do
+
+end
