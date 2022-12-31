0052bf2572501a920baada7eedb53f2c15dd954e
Juan Andrew _test_ <juandreww@outlook.com>
2022-12-31 13:11:57 +0700

    asdfasf

--- spec/change_matcher_spec.rb
+++ spec/change_matcher_spec.rb
@@ -1,12 +1,25 @@
 RSpec.describe 'change matcher' do
-  ash_pokemons = ['pikachu', 'charizard', 'kingler', 'snorlax']
+  let(:ash_pokemons) {['pikachu', 'charizard', 'kingler', 'snorlax'] }
 
-  it 'adds pidgeot to the pokemon list' do
-    expect {
-      ash_pokemons.push('pidgeot')
-    }.to change { ash_pokemons.size }.from(4).to(5)
-    .and change { ash_pokemons.size }.by(1)
-    .and change { ash_pokemons }.from(['pikachu', 'charizard', 'kingler', 'snorlax'])
-                                .to(['pikachu', 'charizard', 'kingler', 'snorlax', 'pidgeot'])
+  context 'add pokemon' do
+    it 'adds pidgeot to the pokemon list' do
+      expect {
+        ash_pokemons.push('pidgeot')
+      }.to change { ash_pokemons.size }.from(4).to(5)
+      .and change { ash_pokemons.size }.by(1)
+      .and change { ash_pokemons }.from(['pikachu', 'charizard', 'kingler', 'snorlax'])
+                                  .to(['pikachu', 'charizard', 'kingler', 'snorlax', 'pidgeot'])
+    end
+  end
+
+  context 'remove pokemon' do
+    it 'deletes snorlax from the pokemon list' do
+      expect {
+        ash_pokemons.pop
+      }.to change { ash_pokemons.size }.from(4).to(3)
+      .and change { ash_pokemons.size }.by(-1)
+      .and change { ash_pokemons }.from(['pikachu', 'charizard', 'kingler', 'snorlax'])
+                                  .to(['pikachu', 'charizard', 'kingler'])
+    end
   end
 end
