bcec9efd74e80ce58fbd6c60b24e9775d6e035d2
Juan Andrew _test_ <juandreww@outlook.com>
2022-12-31 10:49:33 +0700

    asdfasdf

--- spec/not_to_method_spec.rb
+++ spec/not_to_method_spec.rb
@@ -1,3 +1,12 @@
 RSpec.describe 'not_to method' do
-  it 'checks '
+  it 'checks when value do not match' do
+    expect(10).not_to eq(11)
+    expect(11.0).not_to eql(11)
+    expect([1, 2]).not_to eq([2, 1])
+    expect([1, 2]).not_to eql([2, 1])
+    expect([1, 2]).to match_array([2, 1])
+    expect([1, 2, 3.0]).to eq([1, 2, 3])
+    expect([1, 2, 3.0]).to eql([1, 2, 3.0])
+    expect([1, 2]).to contain_exactly(2, 1)
+  end
 end
