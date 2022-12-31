3879f44fc7bf1e6a64745949e24cfbe9f2123347
Juan Andrew _test_ <juandreww@outlook.com>
2022-12-31 13:04:24 +0700

    asdfasdf

--- spec/assignment_8_spec.rb
+++ spec/assignment_8_spec.rb
@@ -1,3 +1,31 @@
 RSpec.describe 'assignment 8' do
+  hash = {}
+  describe hash do
+    it { is_expected.to be_empty }
+  end
 
+  describe [10, 20, 30] do
+    it { is_expected.to all(be_even) }
+  end
+
+  describe [0, 1, 2] do
+    it { is_expected.to all(be >= 0) }
+  end
+
+  context 'be matcher' do
+    it 'should return truthy value' do
+      expect("hello").to be_truthy
+      expect(0).to be_truthy
+      expect(-10).to be_truthy
+      expect([]).to be_truthy
+      expect(99.99).to be_truthy
+      expect(:hello).to be_truthy
+      expect([1, 2, 3]).to be_truthy
+    end
+
+    it 'should return falsey value' do
+      expect(false).to be_falsey
+      expect(nil).to be_falsey
+    end
+  end
 end
