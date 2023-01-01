1b60bb28d8525f6f2000ae4e8826b57ac3267470
Juan Andrew _test_ <juandreww@outlook.com>
2023-01-01 18:56:19 +0700

    asdfasdf

--- spec/respond_to_matcher_spec.rb
+++ spec/respond_to_matcher_spec.rb
@@ -34,7 +34,7 @@ class Clothes
     '74 cm'
   end
 
-  def waist_add_size(requestor, add_size)
+  def waist_add_size(requestor, add_size = 0)
     "#{requestor} requested increase of waist from previously 52 cm will add #{add_size} cm"
   end
 end
@@ -43,9 +43,12 @@ RSpec.describe Clothes do
   it 'confirms that an object can respond to a method' do
     expect(subject).to respond_to(:chest)
     expect(subject).to respond_to(:chest, :length)
+    expect(subject).to respond_to(:waist_add_size).with(2).arguments
   end
 
   it 'confirms that an object cannot respond to a method' do
     expect(subject).not_to respond_to(:global_variables)
   end
+
+  it { is_expected.to respond_to(:waist_add_size).with(2).arguments }
 end
