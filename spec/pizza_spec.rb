c617584026dcd5e6dda0bba15c82d8d1127a3859
Juan Andrew _test_ <juandreww@outlook.com>
2023-01-06 06:35:55 +0700

    asdfasdf

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -1,7 +1,32 @@
 require 'byebug'
 
-class Pizza
-  def initialize
+class DeliveryMan
+  def ready?
+    sleep(3)
+    true
+  end
 
+  def turn_on_vehicle_engine
+    puts 'Turning on vehicle engine.'
+  end
+
+  def delivery_on_the_way
+    puts 'Delivery on the way.'
+  end
+
+  def arrived_at_destination
+    puts 'Arrived at destination.'
+  end
+end
+
+class Pizza
+  attr_reader :delivery_man
+
+  def initialize(delivery_man)
+    @delivery_man = delivery_man
+  end
+
+  def start_delivery
+    if delivery_man.
   end
 end
