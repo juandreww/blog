ebad519bde91331fe07bfc7f76c7c5f3685e31e0
Juan Andrew _test_ <juandreww@outlook.com>
2023-01-06 07:12:00 +0700

    asdasf

---
+++ spec/allow_method_spec.rb
@@ -0,0 +1,19 @@
+require 'byebug'
+
+RSpec.describe 'allow method review' do
+  it 'can customize return value for methods on doubles' do
+    calculator = double
+    allow(calculator).to receive(:add).and_return(15)
+
+    expect(calculator.add).to eq(15)
+    expect(calculator.add(3)).to eq(15)
+    expect(calculator.add(-2, -3, -5)).to eq(15)
+    expect(calculator.add('hello')).to eq(15)
+  end
+
+  it 'can stub one or more methods on a real object' do
+    arr = [1, 2, 3]
+    allow(arr).to receive(:sum).and_return(10)
+    expect(arr.sum).to eq(10)
+  end
+end
