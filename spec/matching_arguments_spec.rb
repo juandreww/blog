455e457c67ee89c0347a818466933c7037cd9edf
Juan Andrew _test_ <juandreww@outlook.com>
2023-01-07 11:23:12 +0700

    asdfasf

--- spec/matching_arguments_spec.rb
+++ spec/matching_arguments_spec.rb
@@ -13,4 +13,35 @@ RSpec.describe 'matching arguments' do
     expect(three_element_array.first(3)).to eq([1, 2, 3])
     expect(three_element_array.first(100)).to eq([1, 2, 3])
   end
+
+  it 'return values depending on the condition given' do
+    tissue_hash = double
+
+    allow(tissue_hash).to receive(:brand).with(no_args).and_return({brand: 'Kodomo Baby'})
+    allow(tissue_hash).to receive(:brand).with('Tessa').and_return({brand: 'Tessa'})
+
+    expect(tissue_hash.brand).to eq(
+      {brand: 'Kodomo Baby'}
+    )
+    expect(tissue_hash.brand('Tessa')).to eq(
+      {brand: 'Tessa'}
+    )
+  end
+
+  it 'return values depending on the condition given #using stub' do
+    tissue_hash = double
+
+    tissue_hash.stub(:brand).and_return({brand: 'Kodomo Baby'})
+    tissue_hash.stub(:brand).with('Tessa').and_return({brand: 'Tessa'})
+
+    allow(tissue_hash).to receive(:brand).with(no_args).and_return({brand: 'Kodomo Baby'})
+    allow(tissue_hash).to receive(:brand).with('Tessa').and_return({brand: 'Tessa'})
+
+    expect(tissue_hash.brand).to eq(
+      {brand: 'Kodomo Baby'}
+    )
+    expect(tissue_hash.brand('Tessa')).to eq(
+      {brand: 'Tessa'}
+    )
+  end
 end
