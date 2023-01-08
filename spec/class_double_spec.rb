b032a638647403cd86caab3c7deaacd262745ecf
Juan Andrew _test_ <juandreww@outlook.com>
2023-01-08 18:30:55 +0700

    asdfasdf

--- spec/class_double_spec.rb
+++ spec/class_double_spec.rb
@@ -12,7 +12,6 @@ class CardGame
   end
 end
 
-
 RSpec.describe CardGame do
   it 'can only implement class methods that are defined on a class' do
     deck_klass = class_double(Deck, build: ['Ace', 'Queen']).as_stubbed_const
