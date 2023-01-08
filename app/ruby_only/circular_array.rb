c36c0dc6d20c2aeddad579a2a351b67ca862ecd0
Juan Andrew _test_ <juandreww@outlook.com>
2023-01-09 06:59:02 +0700

    asdfasdf

---
+++ app/ruby_only/circular_array.rb
@@ -0,0 +1,5 @@
+require "byebug"
+
+domino_pizza_menus = ["grilled beef supreme", "cheesy meatball blast", "ultimate cheese melt", "american frankie sausage",
+                      "american classic cheeseburger"]
+byebug
