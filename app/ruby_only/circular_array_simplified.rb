a7f506acdd1b536827937a7119c84ff41bb5fb04
Juan Andrew _test_ <juandreww@outlook.com>
2023-01-09 22:08:52 +0700

    asdfasdf

---
+++ app/ruby_only/circular_array_simplified.rb
@@ -0,0 +1,15 @@
+def prints(menu, starting_index)
+  menu_size = menu.size
+  i = starting_index
+
+  while i < menu_size + starting_index
+    puts(" ") if i > starting_index
+    puts(menu[(i % menu_size)])
+    i += 1
+  end
+end
+
+domino_pizza_menus = ["0. grilled beef supreme", "1. cheesy meatball blast", "2. ultimate cheese melt",
+                      "3. american frankie sausage", "4. american classic cheeseburger"]
+starting_index = 3
+prints(domino_pizza_menus, starting_index)
