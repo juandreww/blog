3c052816a4e52609a6f84659fb98fd9af67070e0
Juan Andrew _test_ <juandreww@outlook.com>
2023-01-14 08:13:13 +0700

    asdfasdf

--- app/ruby_only/double_linked_lists.rb
+++ app/ruby_only/double_linked_lists.rb
@@ -1,5 +1,6 @@
+# rubocop: disable Rails/Output
 class Node
-  attr_accessor :prev, :next
+  attr_accessor :prev_val, :next_val, :day_name
 
   def initialize(day_name)
     @prev_val = nil
@@ -17,6 +18,14 @@ sat = Node.new("Saturday")
 sun = Node.new("Sunday")
 
 day_names = [mon, tue, wed, thu, fri, sat, sun]
-day_names.each_with_index do |day|
-
+day_names.each_with_index do |day, index|
+  day.prev_val = day_names[index - 1]
+  day.next_val = day_names[index + 1]
 end
+
+puts "After #{mon.day_name}, it will be #{mon.next_val.day_name}"
+puts "Before #{mon.day_name}, it will be #{mon.prev_val.day_name}"
+puts "After #{sat.day_name}, it will be #{sat.next_val.day_name}"
+puts "Before #{sat.day_name}, it will be #{sat.prev_val.day_name}"
+
+# rubocop: enable Rails/Output
