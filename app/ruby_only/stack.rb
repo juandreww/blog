535b7d36f81fce354599574782015e7ccc3fde53
Juan Andrew _test_ <juandreww@outlook.com>
2023-01-16 07:18:11 +0700

    asdfasf

--- app/ruby_only/stack.rb
+++ app/ruby_only/stack.rb
@@ -1,21 +1,20 @@
-class Stack
-  def create_stack
-    []
-  end
-
-  def push(stack, item)
-    stack << item
-    puts "#{item} is pushed to the stack"
-  end
-
-  def remove(stack)
-    stack.pop
-  end
+def create_stack
+  []
 end
 
-stack_klass = Stack.new
-stack = []
-stack = stack_klass.push(stack, 10)
-stack = stack_klass.push(stack, 20)
-stack = stack_klass.push(stack, 30)
+def push(stack, item)
+  stack << item
+
+  puts "insert #{item} to stack"
+
+  stack
+end
+
+def remove(stack)
+  stack.pop
+end
+stack = create_stack
+stack = push(stack, 10)
+stack = push(stack, 20)
+stack = push(stack, 30)
 puts stack
