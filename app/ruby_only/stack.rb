5c46d95e37984e356fadc5dddfdaa3c2f02926bc
Juan Andrew _test_ <juandreww@outlook.com>
2023-01-16 20:46:52 +0700

    asdfasdf

--- app/ruby_only/stack.rb
+++ app/ruby_only/stack.rb
@@ -13,9 +13,14 @@ end
 
 def remove(stack)
   stack.pop
+
+  stack
 end
+
 stack = create_stack
 stack = push(stack, 10)
 stack = push(stack, 20)
 stack = push(stack, 30)
-puts stack
+stack = remove(stack)
+puts stack.inspect
+puts stack == [10, 20]
