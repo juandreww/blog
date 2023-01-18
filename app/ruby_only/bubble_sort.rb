e20ed016ac86e5cc447a6e1e4c876be1a82db531
Juan Andrew _test_ <juandreww@outlook.com>
2023-01-18 07:28:40 +0700

    bubble sort

--- app/ruby_only/bubble_sort.rb
+++ app/ruby_only/bubble_sort.rb
@@ -1,14 +1,18 @@
 def bubble_sort(arr)
-  arr_size = arr.size
+  arr_size = arr.size - 1
 
-  arr_size.times do |i|
-    (arr_size - i).times do |j|
-      if arr[i] > arr[i + j]
+  (0..arr_size).each do |i|
+    (i + 1..arr_size).each do |j|
+      next unless arr[i] > arr[j]
 
-      end
+      tmp = arr[i]
+      arr[i] = arr[j]
+      arr[j] = tmp
     end
   end
+
+  arr
 end
 
 arr = [10, 30, 20, 40, 30, 10]
-bubble_sort(arr)
+puts bubble_sort(arr)
