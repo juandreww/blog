5b74a9032fe8cfa4fcd8a8496171a62216b74828
Juan Andrew _test_ <juandreww@outlook.com>
2023-01-21 11:07:07 +0700

    array inspect

--- app/ruby_only/quick_sort.rb
+++ app/ruby_only/quick_sort.rb
@@ -1,7 +1,10 @@
 def quick_sort(array, low, high)
   return unless low < high
 
+  puts array.inspect
+
   pi = partition(array, low, high)
+  puts pi
   quick_sort(array, low, pi - 1)
   quick_sort(array, pi + 1, high)
 end
@@ -10,7 +13,7 @@ def partition(array, low, high)
   pivot = array[high]
   i = low - 1
 
-  low..high.each do |index|
+  (low..high).each do |index|
     next unless array[index] <= pivot
 
     i += 1
@@ -23,6 +26,9 @@ def partition(array, low, high)
   i
 end
 
-array = [10, 20, 30, 40]
+array = [30, 10, 40, 20]
 puts array.inspect
-quick_sort(array, 0, array.size - 1)
+
+array[0], array[1] = array[1], array[0]
+puts array.inspect
+# quick_sort(array, 0, array.size - 1)
