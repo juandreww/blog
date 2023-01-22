9b133bb7251ba62d1e4e0df5414ede3a66cd597d
Juan Andrew _test_ <juandreww@outlook.com>
2023-01-22 08:08:55 +0700

    asdfasf

--- app/ruby_only/merge_sort.rb
+++ app/ruby_only/merge_sort.rb
@@ -1,18 +1,21 @@
 def divide_array(array)
+  return array if array.size < 2
+
   pivot = array.size / 2
 
-  array_1 = pivot.odd? ? array.first(pivot + 1) : array.first(pivot)
-  array_2 = array.last(pivot)
+  array_left = array.size.odd? ? array.first(pivot + 1) : array.first(pivot)
+  array_right = array.last(pivot)
 
-  puts array_1.inspect
-  puts array_2.inspect
+  puts array_left.inspect
+  puts array_right.inspect
+  puts
 
-  divide_array(array_1)
-  divide_array(array_2)
+  [divide_array(array_left), divide_array(array_right)]
 end
 
 def merge_sort(array)
-  divide_array(array)
+  array_divided = divide_array(array)
+  
 end
 
 array = [38, 27, 9, 12, 18]
