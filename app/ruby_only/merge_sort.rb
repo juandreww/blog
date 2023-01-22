83f7292306e2685e88be25f123bbd07df4276509
Juan Andrew _test_ <juandreww@outlook.com>
2023-01-22 08:03:23 +0700

    asdfasf

--- app/ruby_only/merge_sort.rb
+++ app/ruby_only/merge_sort.rb
@@ -1,9 +1,14 @@
 def divide_array(array)
   pivot = array.size / 2
-  array_1 = array.first(pivot + 1)
+
+  array_1 = pivot.odd? ? array.first(pivot + 1) : array.first(pivot)
   array_2 = array.last(pivot)
+
   puts array_1.inspect
   puts array_2.inspect
+
+  divide_array(array_1)
+  divide_array(array_2)
 end
 
 def merge_sort(array)
