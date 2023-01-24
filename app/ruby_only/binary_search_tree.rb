9dba3c6b4bfc5a83a05747a9a3e771b099148e97
Juan Andrew _test_ <juandreww@outlook.com>
2023-01-24 07:21:09 +0700

    asdfasdf

--- app/ruby_only/binary_search_tree.rb
+++ app/ruby_only/binary_search_tree.rb
@@ -7,3 +7,13 @@ class Node
 
   attr_accessor :number, :left, :right
 end
+
+def max_value(node)
+  return 0 if node.empty?
+end
+
+def min_value(node)
+  return 1_000_000_000 if node.empty?
+end
+
+
