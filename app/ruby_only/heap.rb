7f64648d1eedf4b068d52616a48099cb52de30ae
Juan Andrew _test_ <juandreww@outlook.com>
2023-01-30 07:18:31 +0700

    asdadsf

---
+++ app/ruby_only/heap.rb
@@ -0,0 +1,8 @@
+require "Heap"
+
+b_heap = Heap::BinaryHeap::MinHeap.new([2, 3, 1, -1])
+print("\n")
+print("#{b_heap.elements}\n")
+print("#{b_heap.class}\n")
+print("#{b_heap.extract_min}\n")
+print("\n")
