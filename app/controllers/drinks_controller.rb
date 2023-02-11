7c79ae26997919842903d0ec4900f79151bda15f
Juan Andrew <juandreww108@gmail.com>
2023-02-11 16:25:42 +0700

    parameter drink

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -5,6 +5,18 @@ class DrinksController < ApplicationController
   end
 
   def create
-    byebug
+    @drink = Drink.new(create_params)
+
+    if @drink.save
+      redirect_to @drink
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def create_params
+    @create_params ||= params["drink"].permit(:brand, :name, :unit)
   end
 end
